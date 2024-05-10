54ee43d9e804e13579435b488b87326c43378917
Shirin Kydralieva <kydralievashirin1@gmail.com>
2024-05-10 18:26:30 +0600

    Initial commit

---
+++ src/main/java/itacademy/misbackend/MisBackendApplication.java
@@ -0,0 +1,13 @@
+package itacademy.misbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MisBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/itacademy/misbackend/MisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package itacademy.misbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MisBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
