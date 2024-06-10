60039ddc98dcbfebedf05b619566157b5ae7ec4e
Shirin Kydralieva <kydralievashirin1@gmail.com>
2024-06-10 13:09:57 +0600

    исправила параметр в методе

--- src/main/java/itacademy/misbackend/config/RabbitMQConfig.java
+++ src/main/java/itacademy/misbackend/config/RabbitMQConfig.java
@@ -1,7 +1,7 @@
 package itacademy.misbackend.config;
 
-import com.rabbitmq.client.ConnectionFactory;
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
