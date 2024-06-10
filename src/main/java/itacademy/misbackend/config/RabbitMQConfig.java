3f2ba64454d9f0bbf7ef33195ab53a997af2a7c1
Shirin Kydralieva <kydralievashirin1@gmail.com>
2024-06-10 17:17:57 +0600

    исправления

--- src/main/java/itacademy/misbackend/config/RabbitMQConfig.java
+++ src/main/java/itacademy/misbackend/config/RabbitMQConfig.java
@@ -1,14 +1,9 @@
 package itacademy.misbackend.config;
 
-import org.springframework.amqp.core.*;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+import org.springframework.beans.factory.annotation.Value;
+
+//@Configuration
 public class RabbitMQConfig {
     @Value("${rabbitmq.queue.email.name}")
     private String emailQueue;
@@ -19,7 +14,7 @@ public class RabbitMQConfig {
     @Value("${rabbitmq.binding.email.name}")
     private String emailRoutingKey;
 
-    @Bean
+    /*@Bean
     public Queue emailQueue() {
         return new Queue(emailQueue);
     }
@@ -47,4 +42,5 @@ public class RabbitMQConfig {
     public Jackson2JsonMessageConverter converter() {
         return new Jackson2JsonMessageConverter();
     }
+     */
 }
--- src/main/java/itacademy/misbackend/service/impl/UserServiceImpl.java
+++ src/main/java/itacademy/misbackend/service/impl/UserServiceImpl.java
@@ -23,6 +23,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -53,8 +54,8 @@ public class UserServiceImpl implements UserService, UserDetailsService {
     }
     @Override
     public UserDto save(UserDto userDto) {
-        userDto.setPassword(passwordEncoder.encode(userDto.getPassword()));
         log.info("СТАРТ: UserServiceImpl - create() {}", userDto);
+        userDto.setPassword(passwordEncoder.encode(userDto.getPassword()));
         User user = userMapper.toEntity(userDto);
         String token = UUID.randomUUID().toString();
         var roles = new HashSet<Role>();
